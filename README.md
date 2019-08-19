033ef7802e3c422e4699523e302a346c0983df59
Dylan Barratt <dylanbarratt@catlover.com>
2019-08-19 17:51:44 +0100

    FINISHED IT!

---
+++ README.md
@@ -0,0 +1,11 @@
+# Unbeatable Noughts & Crosses
+
+## Table of Contents
+
+- [About](#about)
+
+## About <a name = "about"></a>
+
+An simple game of naughts and crosses. Only, YOU CANNOT WIN. If you do win however please contact me on github with how and I will not remove this victory.
+
+Go to <a href ="dylanbarratt.com/projects/Noughts & crosses">dylanbarratt.com</a> to see it in action!
--- app.js
+++ app.js
@@ -63,6 +63,25 @@ function tileInit() {
 }
 
 function tileDraw() {
+	if (!winCheck()) {
+		for (i = 0; i < tiles.length; i++) {
+			if (mouseX >= tiles[i][0] && mouseX <= tiles[i][0] + tiles[i][2] && mouseY >= tiles[i][1] && mouseY <= tiles[i][1] + tiles[i][3]) {
+				cursor("pointer");
+				if (mouseIsPressed && started && tiles[i][4] == "" && turn) {
+					drawCircle(i);
+					turn = false;
+					move += 1;
+				}
+				if (!turn) {
+					var place;
+					place = chooseMove(i);
+					drawCross(place);
+					turn = true;
+					move += 1;
+				}
+			}
+		}
+	}
 	for (i = 0; i < tiles.length; i++) {
 		if (mouseX >= tiles[i][0] && mouseX <= tiles[i][0] + tiles[i][2] && mouseY >= tiles[i][1] && mouseY <= tiles[i][1] + tiles[i][3]) {
 			cursor("pointer");
@@ -72,7 +91,10 @@ function tileDraw() {
 				move += 1;
 			}
 			if (!turn) {
-				chooseMove();
+				var place;
+				place = chooseMove(i);
+				drawCross(place);
+				turn = true;
 				move += 1;
 			}
 		}
@@ -80,30 +102,34 @@ function tileDraw() {
 }
 
 function drawCircle(i) {
-	tiles[i][4] = "O";
-	lastPlaced = i;
+	if (!winCheck()) {
+		tiles[i][4] = "O";
+		lastPlaced = i;
 
-	circles.push([tiles[i][0] + tiles[i][2] / 2, tiles[i][1] + tiles[i][3] / 2, tiles[i][3] - 35]);
+		circles.push([tiles[i][0] + tiles[i][2] / 2, tiles[i][1] + tiles[i][3] / 2, tiles[i][3] - 35]);
+	}
 }
 
 function drawCross(i) {
-	if (tiles[i][4] == "") {
-		tiles[i][4] = "X";
+	if (!winCheck()) {
+		if (tiles[i][4] == "") {
+			tiles[i][4] = "X";
 
-		crosses.push([
-			//LINE 1 \
-			tiles[i][0] + 30,
-			tiles[i][1] + 30,
-			tiles[i][0] + tiles[i][2] - 30,
-			tiles[i][1] + tiles[i][3] - 30,
-			//LINE 2 /
-			tiles[i][0] + tiles[i][2] - 30,
-			tiles[i][1] + 30,
-			tiles[i][0] + 30,
-			tiles[i][1] + tiles[i][3] - 30
-		]);
-	} else {
-		alert("ERROR PLACING X AT " + i);
+			crosses.push([
+				//LINE 1 \
+				tiles[i][0] + 30,
+				tiles[i][1] + 30,
+				tiles[i][0] + tiles[i][2] - 30,
+				tiles[i][1] + tiles[i][3] - 30,
+				//LINE 2 /
+				tiles[i][0] + tiles[i][2] - 30,
+				tiles[i][1] + 30,
+				tiles[i][0] + 30,
+				tiles[i][1] + tiles[i][3] - 30
+			]);
+		} else {
+			alert("ERROR PLACING X AT " + i);
+		}
 	}
 }
 
@@ -117,69 +143,90 @@ function shapeDraw() {
 		line(crosses[i][0], crosses[i][1], crosses[i][2], crosses[i][3]);
 		line(crosses[i][4], crosses[i][5], crosses[i][6], crosses[i][7]);
 	}
+}
 
-	winCheck();
+function finalDraw() {
+	noFill();
+	stroke(0);
+	for (i = 0; i < circles.length; i++) {
+		circle(circles[i][0], circles[i][1], circles[i][2]);
+	}
+	for (i = 0; i < crosses.length; i++) {
+		line(crosses[i][0], crosses[i][1], crosses[i][2], crosses[i][3]);
+		line(crosses[i][4], crosses[i][5], crosses[i][6], crosses[i][7]);
+	}
 }
 
 function winCheck() {
-	//HORIZONTAL CHECKS
-	if (tiles[0][4] == tiles[1][4] && tiles[0][4] == tiles[2][4] && tiles[0][4] != "") {
-		alert(tiles[0][4] + " WINS HORIZONTAL 1 IN " + move + " moves");
-		started = false;
-		tileInit();
-	}
-	if (tiles[3][4] == tiles[4][4] && tiles[3][4] == tiles[5][4] && tiles[3][4] != "") {
-		alert(tiles[3][4] + " WINS HORIZONTAL 2" + move + " moves");
-		started = false;
-		tileInit();
-	}
-	if (tiles[6][4] == tiles[7][4] && tiles[6][4] == tiles[8][4] && tiles[6][4] != "") {
-		alert(tiles[6][4] + " WINS HORIZONTAL 3" + move + " moves");
-		started = false;
-		tileInit();
-	}
-
-	//VERTICAL CHECKS
-	if (tiles[0][4] == tiles[3][4] && tiles[0][4] == tiles[6][4] && tiles[0][4] != "") {
-		alert(tiles[0][4] + " WINS VERTICAL 1" + move + " moves");
-		started = false;
-		tileInit();
-	}
-	if (tiles[1][4] == tiles[4][4] && tiles[1][4] == tiles[7][4] && tiles[1][4] != "") {
-		alert(tiles[1][4] + " WINS VERTICAL 2" + move + " moves");
-		started = false;
-		tileInit();
-	}
-	if (tiles[2][4] == tiles[5][4] && tiles[2][4] == tiles[8][4] && tiles[2][4] != "") {
-		alert(tiles[2][4] + " WINS VERTICAL 3" + move + " moves");
-		started = false;
-		tileInit();
-	}
-
-	//DIAGONAL CHECKS
-	if (tiles[0][4] == tiles[4][4] && tiles[0][4] == tiles[8][4] && tiles[0][4] != "") {
-		alert(tiles[0][4] + " WINS DIAGONAL 1" + move + " moves");
-		started = false;
-		tileInit();
-	}
-	if (tiles[2][4] == tiles[4][4] && tiles[2][4] == tiles[6][4] && tiles[2][4] != "") {
-		alert(tiles[2][4] + " WINS DIAGONAL 2" + move + " moves");
-		started = false;
-		tileInit();
-	}
-
-	//FULL CHECK
-	var counter = 0;
-	for (i = 0; i < 8; i++) {
-		if (tiles[i][4] != "") {
-			counter += 1;
+	if (started) {
+		//HORIZONTAL CHECKS
+		if (tiles[0][4] == tiles[1][4] && tiles[0][4] == tiles[2][4] && tiles[0][4] != "") {
+			started = false;
+			finalDraw();
+			alert(tiles[0][4] + " WINS HORIZONTAL 1 (press reset to start again)");
+			return true;
 		}
-	}
+		if (tiles[3][4] == tiles[4][4] && tiles[3][4] == tiles[5][4] && tiles[3][4] != "") {
+			started = false;
+			finalDraw();
+			alert(tiles[3][4] + " WINS HORIZONTAL 2 (press reset to start again)");
+			return true;
+		}
+		if (tiles[6][4] == tiles[7][4] && tiles[6][4] == tiles[8][4] && tiles[6][4] != "") {
+			started = false;
+			finalDraw();
+			alert(tiles[6][4] + " WINS HORIZONTAL 3 (press reset to start again)");
+			return true;
+		}
 
-	if (counter == 8) {
-		alert("TIE");
-		started = false;
-		tileInit();
+		//VERTICAL CHECKS
+		if (tiles[0][4] == tiles[3][4] && tiles[0][4] == tiles[6][4] && tiles[0][4] != "") {
+			started = false;
+			finalDraw();
+			alert(tiles[0][4] + " WINS VERTICAL 1 (press reset to start again)");
+			return true;
+		}
+		if (tiles[1][4] == tiles[4][4] && tiles[1][4] == tiles[7][4] && tiles[1][4] != "") {
+			started = false;
+			finalDraw();
+			alert(tiles[1][4] + " WINS VERTICAL 2 (press reset to start again)");
+			return true;
+		}
+		if (tiles[2][4] == tiles[5][4] && tiles[2][4] == tiles[8][4] && tiles[2][4] != "") {
+			alert(tiles[2][4] + " WINS VERTICAL 3 (press reset to start again)");
+			started = false;
+			finalDraw();
+			return true;
+		}
+
+		//DIAGONAL CHECKS
+		if (tiles[0][4] == tiles[4][4] && tiles[0][4] == tiles[8][4] && tiles[0][4] != "") {
+			started = false;
+			finalDraw();
+			alert(tiles[0][4] + " WINS DIAGONAL 1 (press reset to start again)");
+			return true;
+		}
+		if (tiles[2][4] == tiles[4][4] && tiles[2][4] == tiles[6][4] && tiles[2][4] != "") {
+			started = false;
+			finalDraw();
+			alert(tiles[2][4] + " WINS DIAGONAL 2 (press reset to start again)");
+			return true;
+		}
+
+		//FULL CHECK
+		var counter = 0;
+		for (i = 0; i < tiles.length; i++) {
+			if (tiles[i][4] != "") {
+				counter += 1;
+			}
+		}
+
+		if (counter >= 8) {
+			alert("TIE (press reset to start again)");
+			started = false;
+			finalDraw();
+			return true;
+		}
 	}
 }
 
@@ -188,11 +235,6 @@ function mouseClicked() {
 }
 
 //0 = corner start || 1 = middle start || 2 = side start
-/*
-drawCross(4);
-turn = true;
-move += 1;
-*/
 
 var start = 0;
 function chooseMove() {
@@ -200,112 +242,253 @@ function chooseMove() {
 
 	if (move == 1) {
 		if (tiles[0][4] == "O" || tiles[2][4] == "O" || tiles[6][4] == "O" || tiles[8][4] == "O") {
-			drawCross(4);
-			turn = true;
-			move += 1;
 			start = 0;
+			var r = 4;
+			return r;
+		} else if (tiles[1][4] == "O" || tiles[3][4] == "O" || tiles[5][4] == "O" || tiles[7][4] == "O") {
+			start = 2;
+			var r = 4;
+			return r;
 		}
 	}
 
-	//CORNER START
+	//CHECK TO WIN
+	//HORIZONTAL 1
+	if (tiles[0][4] == "X" && tiles[1][4] == "X" && tiles[2][4] == "") {
+		var r = 2;
+		return r;
+	} else if (tiles[0][4] == "X" && tiles[2][4] == "X" && tiles[1][4] == "") {
+		var r = 1;
+		return r;
+	} else if (tiles[1][4] == "X" && tiles[2][4] == "X" && tiles[0][4] == "") {
+		var r = 0;
+		return r;
+	}
 
-	if (move == 4 && start == 0) {
-		if (tiles[0][4] == "O" && tiles[2][4] == "O") {
-			drawCross(1);
-			turn = true;
-			move += 1;
-		} else if (tiles[0][4] == "O" && tiles[6][4] == "O") {
-			drawCross(3);
-			turn = true;
-			move += 1;
-		} else if (tiles[2][4] == "O" && tiles[0][4] == "O") {
-			drawCross(1);
-			turn = true;
-			move += 1;
-		} else if (tiles[2][4] == "O" && tiles[8][4] == "O") {
-			drawCross(5);
-			turn = true;
-			move += 1;
-		} else if (tiles[6][4] == "O" && tiles[0][4] == "O") {
-			drawCross(3);
-			turn = true;
-			move += 1;
-		} else if (tiles[6][4] == "O" && tiles[8][4] == "O") {
-			drawCross(7);
-			turn = true;
-			move += 1;
-		} else if (tiles[0][4] == "O" && tiles[8][4] == "O") {
-			drawCross(7);
-			turn = true;
-			move += 1;
-		} else if (tiles[2][4] == "O" && tiles[6][4] == "O") {
-			drawCross(7);
-			turn = true;
-			move += 1;
-		} else {
-			if (tiles[1][4] == "") {
-				drawCross(1);
-				turn = true;
-				move += 1;
-			} else if (tiles[3][4] == "") {
-				drawCross(3);
-				turn = true;
-				move += 1;
-			} else if (tiles[5][4] == "") {
-				drawCross(5);
-				turn = true;
-				move += 1;
-			} else if (tiles[7][4] == "") {
-				drawCross(7);
-				turn = true;
-				move += 1;
-			}
+	//HORIZONTAL 2
+	if (tiles[3][4] == "X" && tiles[4][4] == "X" && tiles[5][4] == "") {
+		var r = 5;
+		return r;
+	} else if (tiles[3][4] == "X" && tiles[5][4] == "X" && tiles[4][4] == "") {
+		var r = 4;
+		return r;
+	} else if (tiles[4][4] == "X" && tiles[5][4] == "X" && tiles[3][4] == "") {
+		var r = 3;
+		return r;
+	}
+
+	//HORIZONTAL 3
+	if (tiles[6][4] == "X" && tiles[7][4] == "X" && tiles[8][4] == "") {
+		var r = 8;
+		return r;
+	} else if (tiles[6][4] == "X" && tiles[8][4] == "X" && tiles[7][4] == "") {
+		var r = 7;
+		return r;
+	} else if (tiles[7][4] == "X" && tiles[8][4] == "X" && tiles[6][4] == "") {
+		var r = 6;
+		return r;
+	}
+
+	//VERTICAL 1
+	if (tiles[0][4] == "X" && tiles[3][4] == "X" && tiles[6][4] == "") {
+		var r = 6;
+		return r;
+	} else if (tiles[0][4] == "X" && tiles[6][4] == "X" && tiles[3][4] == "") {
+		var r = 3;
+		return r;
+	} else if (tiles[3][4] == "X" && tiles[6][4] == "X" && tiles[0][4] == "") {
+		var r = 0;
+		return r;
+	}
+
+	//VERTICAL 2
+	if (tiles[1][4] == "X" && tiles[4][4] == "X" && tiles[7][4] == "") {
+		var r = 7;
+		return r;
+	} else if (tiles[1][4] == "X" && tiles[7][4] == "X" && tiles[4][4] == "") {
+		var r = 4;
+		return r;
+	} else if (tiles[4][4] == "X" && tiles[7][4] == "X" && tiles[1][4] == "") {
+		var r = 1;
+		return r;
+	}
+
+	//VERTICAL 3
+	if (tiles[2][4] == "X" && tiles[5][4] == "X" && tiles[8][4] == "") {
+		var r = 8;
+		return r;
+	} else if (tiles[2][4] == "X" && tiles[8][4] == "X" && tiles[5][4] == "") {
+		var r = 5;
+		return r;
+	} else if (tiles[5][4] == "X" && tiles[8][4] == "X" && tiles[2][4] == "") {
+		var r = 2;
+		return r;
+	}
+
+	//DIAGONAL 1
+	if (tiles[0][4] == "X" && tiles[4][4] == "X" && tiles[8][4] == "") {
+		var r = 8;
+		return r;
+	} else if (tiles[0][4] == "X" && tiles[8][4] == "X" && tiles[4][4] == "") {
+		var r = 4;
+		return r;
+	} else if (tiles[4][4] == "X" && tiles[8][4] == "X" && tiles[0][4] == "") {
+		var r = 0;
+		return r;
+	}
+
+	//DIAGONAL 2
+	if (tiles[2][4] == "X" && tiles[4][4] == "X" && tiles[6][4] == "") {
+		var r = 6;
+		return r;
+	} else if (tiles[2][4] == "X" && tiles[6][4] == "X" && tiles[4][4] == "") {
+		var r = 4;
+		return r;
+	} else if (tiles[4][4] == "X" && tiles[6][4] == "X" && tiles[2][4] == "") {
+		var r = 2;
+		return r;
+	}
+
+	//CHECK TO BLOCK
+	//HORIZONTAL 1
+	if (tiles[0][4] == "O" && tiles[1][4] == "O" && tiles[2][4] == "") {
+		console.log("h1");
+		var r = 2;
+		return r;
+	} else if (tiles[0][4] == "O" && tiles[2][4] == "O" && tiles[1][4] == "") {
+		console.log("h1");
+		var r = 1;
+		return r;
+	} else if (tiles[1][4] == "O" && tiles[2][4] == "O" && tiles[0][4] == "") {
+		console.log("h1");
+		var r = 0;
+		return r;
+	}
+
+	//HORIZONTAL 2
+	if (tiles[3][4] == "O" && tiles[4][4] == "O" && tiles[5][4] == "") {
+		console.log("h2");
+		var r = 5;
+		return r;
+	} else if (tiles[3][4] == "O" && tiles[5][4] == "O" && tiles[4][4] == "") {
+		console.log("h2");
+		var r = 4;
+		return r;
+	} else if (tiles[4][4] == "O" && tiles[5][4] == "O" && tiles[3][4] == "") {
+		console.log("h2");
+		var r = 3;
+		return r;
+	}
+
+	//HORIZONTAL 3
+	if (tiles[6][4] == "O" && tiles[7][4] == "O" && tiles[8][4] == "") {
+		console.log("h3");
+		var r = 8;
+		return r;
+	} else if (tiles[6][4] == "O" && tiles[8][4] == "O" && tiles[7][4] == "") {
+		console.log("h3");
+		var r = 7;
+		return r;
+	} else if (tiles[7][4] == "O" && tiles[8][4] == "O" && tiles[6][4] == "") {
+		console.log("h3");
+		var r = 6;
+		return r;
+	}
+
+	//VERTICAL 1
+	if (tiles[0][4] == "O" && tiles[3][4] == "O" && tiles[6][4] == "") {
+		console.log("v1");
+		var r = 6;
+		return r;
+	} else if (tiles[0][4] == "O" && tiles[6][4] == "O" && tiles[3][4] == "") {
+		console.log("v1");
+		var r = 3;
+		return r;
+	} else if (tiles[3][4] == "O" && tiles[6][4] == "O" && tiles[0][4] == "") {
+		console.log("v1");
+		var r = 0;
+		return r;
+	}
+
+	//VERTICAL 2
+	if (tiles[1][4] == "O" && tiles[4][4] == "O" && tiles[7][4] == "") {
+		console.log("v2");
+		var r = 7;
+		return r;
+	} else if (tiles[1][4] == "O" && tiles[7][4] == "O" && tiles[4][4] == "") {
+		console.log("v2");
+		var r = 4;
+		return r;
+	} else if (tiles[4][4] == "O" && tiles[7][4] == "O" && tiles[1][4] == "") {
+		console.log("v2");
+		var r = 1;
+		return r;
+	}
+
+	//VERTICAL 3
+	if (tiles[2][4] == "O" && tiles[5][4] == "O" && tiles[8][4] == "") {
+		console.log("v3");
+		var r = 8;
+		return r;
+	} else if (tiles[2][4] == "O" && tiles[8][4] == "O" && tiles[5][4] == "") {
+		console.log("v3");
+		var r = 5;
+		return r;
+	} else if (tiles[5][4] == "O" && tiles[8][4] == "O" && tiles[2][4] == "") {
+		console.log("v3");
+		var r = 2;
+		return r;
+	}
+
+	//DIAGONAL 1
+	if (tiles[0][4] == "O" && tiles[4][4] == "O" && tiles[8][4] == "") {
+		console.log("d1");
+		var r = 8;
+		return r;
+	} else if (tiles[0][4] == "O" && tiles[8][4] == "O" && tiles[4][4] == "") {
+		console.log("d1");
+		var r = 4;
+		return r;
+	} else if (tiles[4][4] == "O" && tiles[8][4] == "O" && tiles[0][4] == "") {
+		console.log("d1");
+		var r = 0;
+		return r;
+	}
+
+	//DIAGONAL 2
+	if (tiles[2][4] == "O" && tiles[4][4] == "O" && tiles[6][4] == "") {
+		console.log("d2");
+		var r = 6;
+		return r;
+	} else if (tiles[2][4] == "O" && tiles[6][4] == "O" && tiles[4][4] == "") {
+		console.log("d2");
+		var r = 4;
+		return r;
+	} else if (tiles[4][4] == "O" && tiles[6][4] == "O" && tiles[2][4] == "") {
+		console.log("d2");
+		var r = 2;
+		return r;
+	}
+
+	if (move == 3) {
+		if (tiles[0][4] == "O" && tiles[7][4] == "O" && tiles[3][4] == "") {
+			var r = 3;
+			return r;
+		} else if (tiles[2][4] == "O" && tiles[7][4] == "O" && tiles[5][4] == "") {
+			var r = 5;
+			return r;
+		} else if (tiles[6][4] == "O" && tiles[2][4] == "O" && tiles[3][4] == "") {
+			var r = 3;
+			return r;
+		} else if (tiles[8][4] == "O" && tiles[1][4] == "O" && tiles[5][4] == "") {
+			var r = 5;
+			return r;
 		}
 	}
 
-	if (move == 7 && start == 0) {
-		if (lastPlaced == 0 && tiles[2][4] == "O" && tiles[1][4] != "X") {
-			drawCross(1);
-			turn = true;
-			move += 1;
-		} else if (lastPlaced == 0 && tiles[6][4] == "O" && tiles[3][4] != "X") {
-			drawCross(3);
-			turn = true;
-			move += 1;
-		} else if (lastPlaced == 2 && tiles[0][4] == "O" && tiles[1][4] != "X") {
-			drawCross(1);
-			turn = true;
-			move += 1;
-		} else if (lastPlaced == 2 && tiles[8][4] == "O" && tiles[5][4] != "X") {
-			drawCross(5);
-			turn = true;
-			move += 1;
-		} else if (lastPlaced == 6 && tiles[0][4] == "O" && tiles[3][4] != "X") {
-			drawCross(3);
-			turn = true;
-			move += 1;
-		} else if (lastPlaced == 6 && tiles[8][4] == "O" && tiles[7][4] != "X") {
-			drawCross(7);
-			turn = true;
-			move += 1;
-		} else {
-			if (tiles[1][4] == "") {
-				drawCross(1);
-				turn = true;
-				move += 1;
-			} else if (tiles[3][4] == "") {
-				drawCross(3);
-				turn = true;
-				move += 1;
-			} else if (tiles[5][4] == "") {
-				drawCross(5);
-				turn = true;
-				move += 1;
-			} else if (tiles[7][4] == "") {
-				drawCross(7);
-				turn = true;
-				move += 1;
-			}
+	for (i = 0; i < tiles.length; i++) {
+		if (tiles[i][4] == "") {
+			return i;
 		}
 	}
 }
